7373b77a5cee02f1562023a171fd82f79bce0186
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-05-30 11:09:54 +0530

    Updated json parse error logging

--- src/utils/parse-response-json.ts
+++ src/utils/parse-response-json.ts
@@ -1,7 +1,22 @@
 import { fromPromise } from "neverthrow";
+import { GenericError, toGenericError } from "../types/generic-error";
 
 export function parseResponseJSON(response: Response) {
-    return fromPromise(response.json(), (error) => {
-        return error as DOMException | TypeError | SyntaxError;
-    });
+    return fromPromise(
+        (async () => {
+            const text = await response.text();
+
+            try {
+                return JSON.parse(text);
+            } catch (error) {
+                throw new GenericError("JSON_PARSE", "Failed to parse response body as json", {
+                    cause: toGenericError(error as DOMException | TypeError | SyntaxError),
+                    responseBody: text,
+                });
+            }
+        })(),
+        (error) => {
+            return error as GenericError;
+        },
+    );
 }
